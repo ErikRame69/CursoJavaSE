9be0004d461d9e518b1e79f2df6b5ddf5310951d
Erik Daniel <erikrame69@gmail.com>
2021-07-13 18:20:40 -0500

    Declarando Arreglos

---
+++ Java8/Arrays/Arrays.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersion = new String[];
+        String days[] = new String[];
+
+        //Array de dos dimensiones
+        String[][] cities = new String[4][2];
+
+        /**
+         * +-------------+-------------+
+         * |  Country    |  City       |
+         * +-------------+-------------+
+         * |  México     | CDMX        |
+         * |  México     | Guadalajara | 
+         * |  Colombia   | Bogotá      |
+         * |  Colombia   | Medellín    |
+         * |             |             |
+         * +-------------+-------------+    
+         */
+
+         int [][][] numbers = new String[2][2][2];
+         int [][][][] numbers4 = new int [][][][];
+    }
+
+    /*Esto es una limitante, no siempre en nuestro projecto vamos a usar arrays, 
+    si nos ayuda con problemas de logicas o espacio pero el problema principal 
+    del array es que conforme pase el tiempo no podra ir creciendo.*/
+}
