a058f8486f5e56534a66cbaaa9811bf95cb43def
Erik Daniel <erikrame69@gmail.com>
2021-07-13 14:02:04 -0500

    Operadores Lógicos y Expresiones booleanas

---
+++ Java8/LogicOperations/LogicOperations.java
@@ -0,0 +1,40 @@
+package LogicOperations;
+
+public class LogicOperations 
+{
+    
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+        //Operadores de asignación
+        System.out.println("¿a es igual a b? ->" + (a == b));
+        System.out.println("¿a es diferente a b? ->" + (a != b));
+
+        //Operadores Relacinales
+        System.out.println("¿a es meyor a b? ->" + (a > b));
+        System.out.println("¿a es menor a b? ->" + (a < b));
+        System.out.println("¿a es meyor o igual a b? ->" + (a >= b));
+        System.out.println("¿a es menor o igual a b? ->" + (a <= b));
+
+        if (a == b) 
+        {
+            System.out.println("a es igual a b");}
+            else if ((a != b) && (a>b)){
+                System.out.println("a es diferente a b");
+            }
+            else if (a > b){
+                System.out.println("a es mayor a b");
+            }
+            else if (a < b){
+                System.out.println("a es menor a b");
+            }
+            else if (a >= b){
+                System.out.println("a mayor o igual a b");
+            }
+            else if (a <= b){
+                System.out.println("a es menor o igual a b");
+            }
+        
+        
+    }
+}
