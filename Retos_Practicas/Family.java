98be7cd6e15605d47c3a4e5ed66f3252cbd3a7e1
Erik Daniel <erikrame69@gmail.com>
2021-07-13 05:58:11 -0500

    Ejercicio 1

---
+++ Retos_Practicas/Family.java
@@ -0,0 +1,54 @@
+public class Family {
+
+    public static void main(String[] args) {
+        
+        //Integrantes
+        String NAME_DAD = "Daniel Tovar";
+        String NAME_MOM = "Dolores Mendoza";
+        String NAME_SISTER_1 = "Elizabeth Ramirez";
+        String NAME_SISTER_2 = "Nayeli Ramirez";
+
+        int ageDad = 49;
+        int ageMom= 49;
+        int ageSister_1 = 45;
+        int ageSister_2 = 18;
+
+        String NATIONALITY_DAD = "México";
+        String NATIONALITY_MOM = "México";
+        String NATIONALITY_SISTER_1 = "México";
+        String NATIONALITY_SISTER_2 = "México";
+
+        double statureDad = 1.78;
+        double statureMom = 1.65;
+        double statureSister_1 = 1.67;
+        double statureSister_2 = 1.69;
+
+        
+        System.out.println("____________________________________________");
+
+        System.out.println("Familiar: " + NAME_DAD + "  Su edad es: " + ageDad);
+        System.out.println("Nacio en: " + NATIONALITY_DAD + "  Su estatura es: " + statureDad);
+
+        System.out.println("____________________________________________");
+
+        System.out.println("Familiar: " + NAME_MOM + "  Su edad es: " + ageMom);
+        System.out.println("Nacio en: " + NATIONALITY_MOM + "  Su estatura es: " + statureMom);
+
+        
+        System.out.println("____________________________________________");
+
+        System.out.println("Familiar: " + NAME_SISTER_1 + "  Su edad es: " + ageSister_1);
+        System.out.println("Nacio en: " + NATIONALITY_SISTER_1 + "  Su estatura es: " + statureSister_1);
+
+        
+        System.out.println("____________________________________________");
+
+        System.out.println("Familiar: " + NAME_SISTER_2 + "  Su edad es: " + ageSister_2);
+        System.out.println("Nacio en: " + NATIONALITY_SISTER_2 + "  Su estatura es: " + statureSister_2);
+
+        
+        System.out.println("____________________________________________");
+    }
+
+    
+}
