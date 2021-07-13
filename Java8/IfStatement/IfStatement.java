bd705e66b18bc300ccbe51f7b3b30e8661346a0d
Erik Daniel <erikrame69@gmail.com>
2021-07-13 07:48:33 -0500

    Alcance de las variables y Sentencia ELSE

--- Java8/IfStatement/IfStatement.java
+++ Java8/IfStatement/IfStatement.java
@@ -7,14 +7,20 @@ public class IfStatement {
         //Supongamos que queremos enviar un archivo via bluetooth
         //Debemos de verificar que el bluetooth este prendido
 
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int fileSended = 3;
 
         if (isBluetoothEnabled)
         {
-            fileSended++;
-            System.out.println("Arichivo Enviado   " + "Cantidad de archivos enviados con exito: " + fileSended);
+            fileSended++; 
+            System.out.println("Arichivo Enviado   " + "Cantidad de archivos enviados con exito: " + fileSended);   
         }
+        else
+        {
+             System.out.println("¡Vaya!, parece el bluetooth esta apagado, enciendelo para trasferir los archivos.");
+        }
+        
+        
     }
 
 }
