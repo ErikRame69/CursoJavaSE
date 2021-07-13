d5a2cb5dafd3dcc499571c96d3fe6136fa4d2542
Erik Daniel <erikrame69@gmail.com>
2021-07-13 15:33:22 -0500

    Javadoc en funciones

--- Java8/Funciones/Funciones.java
+++ Java8/Funciones/Funciones.java
@@ -1,8 +1,14 @@
 package Funciones;
 
 public class Funciones {
-    public static void main(String[] args) {
-
+    
+    /**
+     * Clase donde imprimimos en pantala los resultados de las funciones.
+     * @param args argumeto de la clasee
+     */
+    public static void main(String[] args) 
+    
+    {
         double y = 3;
          /*Nos devulve el área de un círculo
         PI * r², donde y es la base y 2 el esponente*/
@@ -19,17 +25,39 @@ public class Funciones {
         System.out.println("PESOS A DOLARES:" + converToDolar(1000, "MXN"));
     }
 
+    /**
+     * Función donde calculamos el área de un círculo
+     * @param r es el radio
+     * @returnNos retorna elresultado de la variable
+     */
     public static double circleArea (double r){
         return Math.PI * Math.pow(r, 2);
     }
 
+    /**
+     * Función donde calculamos el área de una esfera
+     * @param r es el radio
+     * @return retorna el área de la esfera
+     */
     public static double spheraArea (double r){
         return 4 * Math.PI * Math.pow (r, 2);
     }
 
+    /**
+     * Función donde calculamos el volumen de una esfera
+     * @param r es el radio de la esfera
+     * @return nos retorna el valor del volumen de la esfera
+     */
     public static double sphereVolumen (double r){
         return 4/3 * Math.PI * Math.pow(r, 3);
     }
+
+    /**
+     * Descripción: Función que especificando su moned, convierte una cantidad de dinero a dolares.
+     * @param quantity Cantidad de dinero
+     * @param currency Tipo de moneda: Sólo acepta MXN o COP
+     * @return quantity devuelve la cantida en dolares.
+     */
 
     public static double converToDolar (double quantity, String currency){
         //MXN, COP
