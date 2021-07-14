5f3f0d2fb074de5f727533639a0c8b8fa9af816f
Erik Daniel <erikrame69@gmail.com>
2021-07-13 20:56:58 -0500

    Ciclos For anidados

--- Java8/Arrays/Arrays.java
+++ Java8/Arrays/Arrays.java
@@ -1,22 +1,34 @@
 package Arrays;
 
+import java.nio.file.FileSystemAlreadyExistsException;
+
 public class Arrays {
     public static void main(String[] args) {
-        String[] androidVersion = new String[17];
+        String[] androidVersions = new String[17];
         String days[] = new String[7];
 
-        androidVersion [0] = "Apple Pie";
-        androidVersion [1] = "Banana Bread";
-        androidVersion [2] = "Cupcake";
-        androidVersion [3] = "Donut";
+        androidVersions [0] = "Apple Pie";
+        androidVersions [1] = "Banana Bread";
+        androidVersions [2] = "Cupcake";
+        androidVersions [3] = "Donut";
+
+        for (int i=0; i<=androidVersions.length-1; i++)
+        {
+            System.out.println(androidVersions[i]);
+        }
         
-        //¿Cómo accedo a un datos para poder verlo?
+        for (String androidVersion : androidVersions ) 
+        {
+            System.out.println(androidVersion);
+            
+        }
+        /*-¿Cómo accedo a un datos para poder verlo?
         System.out.println("Esta son algunas de las varsiones de java");
         System.out.println(androidVersion[0]);
         System.out.println(androidVersion[1]);
         System.out.println(androidVersion[2]);
         System.out.println(androidVersion[3]);
-        
+        */
         
         //Array de dos dimensiones
         String[][] cities = new String[4][2];
@@ -41,8 +53,16 @@ public class Arrays {
         cities[2][1] = "Guadalajara";
         cities[3][0] = "Mexico";
         cities[3][1] = "CDMX";
+ 
+        for(int i = 0; i < cities.length; i++)
+        {
+            for (int j=0; j<cities[i].length; j++){
+                System.out.println(cities[i][j]);
+            }
+            
+        }
 
-        System.out.println("Ciudades y paises");
+        /*System.out.println("Ciudades y paises");
         System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
@@ -50,7 +70,29 @@ public class Arrays {
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
+
+        for (String[] pair : cities) {
+            for (String name : pair) {
+                System.out.println(name);
+            }
+        }
+
+        /*En que posición se encuentra el changito*/
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1]= "Monkey";
+
+        for (int i = 1; i < animals.length; i++) {
+            for (int j = 0; j < animals.length; j++) {
+                for (int k = 0; k < animals.length; k++) {
+                    for (int l = 1; l < animals.length; l++) {
+                        System.out.println(animals [i][j][k][l]);
+                    }
+                }
+            }
+            
+        }         
+
 
          int [][][] numbers = new int[2][2][2];
          int [][][][] numbers4 = new int [2][2][2][2];
